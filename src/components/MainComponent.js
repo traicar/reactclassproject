bc7f7dd2004ede57b5bac098d25d9d36913472c2
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2021-05-17 13:37:20 -0500

    Breadcrumbs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -31,21 +31,31 @@ class Main extends Component {
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        };
+        }
 
-        return (
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        }  
+
+           return (
             <div>
                 <Header />
-                               <Switch>
+                <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
             </div>
         );
-    };
-}
+    }
 
+}
 export default Main;
