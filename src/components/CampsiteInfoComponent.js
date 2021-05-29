0ab25caa1b4d97efda8013516703588e2e3f4271
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2021-05-29 13:40:14 -0500

    week 4

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,20 +1,92 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Form, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { LocalForm } from 'react-redux-form';
+import { Control, Errors, LocalForm } from 'react-redux-form';
 
-
-
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(values) {
+        this.toggleModal();
+        alert('Submitted' + JSON.stringify(values));
+
+    }
+
     render() {
         return (
-            <LocalForm>
-                <div>
-                    <Button type="submit" outline className="fa-lg fa fa-pencil" color="secondary">{' '}
-                    Submit Comment
-                </Button>
-                </div>
-            </LocalForm>
+
+
+            <div>  <Button type="submit" outline color="secondary" onClick={this.toggleModal}><i className="fa-lg fa fa-pencil">Submit Comment</i></Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleLogin(values)}>
+
+                            <div className="form-group">
+                                <Label>Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Control.select>
+                            </div>
+
+                            <div className="form-group">
+                                <Label>Author</Label>
+                                <Control.text model=".author" id="author" name="author"
+                                    className="form-control" value={this.state.author} validators={{
+                                        required,
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }}
+                                 />
+<Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+
+                            </div>
+
+                            <div className="form-group">
+                                <Label>Text</Label>
+                                <Control.textarea model=".text" id="text" name="text"
+                                    rows="6"
+                                    className="form-control" />
+                            </div>
+                            <Button type="submit" outline color="secondary"> Submit</Button>
+                        </LocalForm>
+
+                    </ModalBody>
+                </Modal >
+            </div>
         );
     }
 }
@@ -46,7 +118,7 @@ function RenderComments({ comments }) {
                     );
                 })
                 }
-             <CommentForm/>
+                <CommentForm />
             </div >
 
         );
