3589f6142462feb76b33c2ee55418d3e370ae21e
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2021-06-02 18:04:00 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -28,7 +28,7 @@ class CommentForm extends Component {
 
     handleLogin(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -102,7 +102,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -116,7 +116,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                     );
                 })
                 }
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+               <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div >
 
         );
@@ -162,9 +162,9 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
-                    />
+                    />     
                 </div>
             </div>
         );
